8a2384c978e80b1f4dadbef85aa6af3b8fd8e09f
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-05 09:46:25 -0700

    added create method

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
 'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const RecipeModel = require('./models/RecipeModel.js');
+const authorize = require('./auth/authorize.js');
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL;
+const app = express();
+
+app.use(cord());
+app.use(express.json());
+app.use(authorize);
+
+mongoose.connect(MONGODB_URL);
+
+// CREATE
+app.post('/recipes', async (request, response) => {
+  try {
+    console.log('POST request: ', request.body);
+    let ingredients = request.body.ingredients;
+    if (!ingredients.length) {
+      response.status(400).send('Please send valid food ingredients');
+    } else {
+      let newRecipe = new RecipeModel({ dishName, cookingSteps, cookingDuration, countryOfOrigin });
+      let recipe = await newRecipe.save();
+      console.log('New recipe created!: ' + recipe);
+      response.json(recipe);
+    }
+  } catch (error) {
+    response.status(400).send('Please send proper ingredient(s): ', error);
+  }
+});
